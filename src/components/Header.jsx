d673dbf07af7e3591d3f3a65a33076971553ea50
SUSocean <igonind.a1@gmail.com>
2023-01-31 16:28:54 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Fill from '../../public/Fill 213.png'
 export default function Header() {
     return (
         <header className="header">
-            <img src="https://github.com/SUSocean/travel-journal/blob/main/public/Fill%20213.png" alt="globe image" />
+            <img src={Fill} />
             <h3 className="header--headline">my travel journal</h3>
         </header>
     )
