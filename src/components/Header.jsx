53a6ab9ac4196c5b5dc0594ebb12a71cf024547b
SUSocean <igonind.a1@gmail.com>
2023-01-31 16:22:29 +0200

    changed homepage and add import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-
+import Fill from '../../public/Fill 213.png'
 export default function Header() {
     return (
         <header className="header">
-            <img src="../public/Fill 213.png" alt="globe image" />
+            <img src="../../public/Fill 213.png" alt="globe image" />
             <h3 className="header--headline">my travel journal</h3>
         </header>
     )
